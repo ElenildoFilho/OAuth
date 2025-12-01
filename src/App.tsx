c20362da9c551ed6a7fd92b7906e9bf13af07adc
Elenildo Filho <elenildovas123@gmail.com>
2025-12-01 20:54:27 -0300

    teste

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App: React.FC = () => {
       try {
         const res = await handleCallback();
         if (res?.access_token) {
+          
           setToken(res.access_token);
         }
       } catch (e) {
